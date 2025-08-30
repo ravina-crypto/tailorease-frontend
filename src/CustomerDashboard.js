af188286eee2944b20f330c3cf6affbef9312bd0
ravina-crypto <kravina6364@gmail.com>
2025-08-30 17:19:29 +0530

    Create CustomerDashboard.js

---
+++ src/CustomerDashboard.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function CustomerDashboard() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>👗 Customer Dashboard</h1>
+      <p>Welcome! You can book tailoring services, track orders, and make payments here.</p>
+    </div>
+  );
+}
+
+export default CustomerDashboard;
