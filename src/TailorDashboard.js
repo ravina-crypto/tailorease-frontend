cf36dbbfb539e3d1364fe0ea567c706ed09c3c66
ravina-crypto <kravina6364@gmail.com>
2025-08-30 17:20:16 +0530

    Create TailorDashboard.js

---
+++ src/TailorDashboard.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function TailorDashboard() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>🧵 Tailor Dashboard</h1>
+      <p>Manage customer orders, update status, and communicate with delivery partners.</p>
+    </div>
+  );
+}
+
+export default TailorDashboard;
